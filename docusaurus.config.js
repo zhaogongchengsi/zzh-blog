79dba5b6df8909a48e083cf933d4b4b1ac987232
zhaogongchengsi <zzh1586169624@163.com>
2022-03-11 11:26:58 +0800

    预备

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,14 +23,12 @@ const config = {
         docs: {
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          editUrl: "https://github.com/zhaogongchengsi/zzh-blog",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          editUrl: "https://github.com/zhaogongchengsi/zzh-blog",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
