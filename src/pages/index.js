f6f115b538324ab0eb453c113c6e376d80501d9d
zhaogongchengsi <zzh1586169624@163.com>
2022-06-17 17:06:05 +0800

    更新首页

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,20 +4,24 @@ import Layout from '@theme/Layout'
 import Link from '@docusaurus/Link'
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
 import styles from './index.module.css'
-import HomepageFeatures from '@site/src/components/HomepageFeatures'
-import BlogBackground from '@site/src/components/bg/index'
 
+const description = `我是程序员五六七，程序员排行榜一万七千三百六十九位，最擅长写屎山代码，一直以优质的服务和亲民的价格深受村民的喜爱，目前的情感状况是单身，不如一起去喝杯东西交流`
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext()
   return (
     <header className={styles.headerBanner}>
-      <img src={`${siteConfig.baseUrl}img/tbs_animated.svg`} className={styles.headerBannerImg} alt="tbs" />
+      <img
+        src={`${siteConfig.baseUrl}img/tbs_animated.svg`}
+        className={styles.headerBannerImg}
+        alt="tbs"
+      />
       <div className={clsx(styles.desc)}>
         <h1>
           {siteConfig.title} <span>的小破站</span>{' '}
         </h1>
-        <h2>{siteConfig.tagline}</h2>
+        {/* <h2>{siteConfig.tagline}</h2> */}
+        <p className={clsx(styles.homeDesc)}>{description}</p>
         <div className={clsx(styles.homelinks)}>
           <Link className={clsx(styles.homeLink)} to="/docs/intro">
             随便看看
@@ -36,11 +40,10 @@ export default function Home() {
   return (
     <Layout
       title={`${siteConfig.title}-- zzhblog, ZZHBLOG, ZZH_BLOG, zzh的个人博客`}
-      description="zzh 的个人博客"
+      description={description}
     >
-      <HomepageHeader />
       <main>
-        <HomepageFeatures />
+        <HomepageHeader />
       </main>
     </Layout>
   )
